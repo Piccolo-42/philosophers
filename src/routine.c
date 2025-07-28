0afe573fae55df2b801ae0d649b116b19e5790d0
stef42 <stef.rvz@gmail.com>
2025-07-28 15:02:17 +0200

    mutex fix

--- src/routine.c
+++ src/routine.c
@@ -6,38 +6,73 @@
 /*   By: sravizza <sravizza@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 10:53:07 by sravizza          #+#    #+#             */
-/*   Updated: 2025/04/19 16:41:55 by sravizza         ###   ########.fr       */
+/*   Updated: 2025/07/28 15:00:20 by sravizza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+// void	old_take_fork(t_philo *philo)
+// {
+// 	pthread_mutex_lock(philo->left_fork);
+// 	ft_print(philo, "has_taken_a_fork");
+// 	if (philo->data->n_philo == 1)
+// 	{
+// 		ft_sleep(philo->data->t_die * 2);
+// 		return ;
+// 	}
+// 	pthread_mutex_lock(philo->right_fork);
+// 	ft_print(philo, "has_taken_a_fork");
+// }
+
 void	take_fork(t_philo *philo)
 {
-	pthread_mutex_lock(philo->left_fork);
+	pthread_mutex_t	*first_fork;
+	pthread_mutex_t	*second_fork;
+
+	if (philo->left_fork < philo->right_fork)
+	{
+		first_fork = philo->left_fork;
+		second_fork = philo->right_fork;
+	}
+	else
+	{
+		first_fork = philo->right_fork;
+		second_fork = philo->left_fork;
+	}
+	pthread_mutex_lock(first_fork);
 	ft_print(philo, "has_taken_a_fork");
 	if (philo->data->n_philo == 1)
 	{
 		ft_sleep(philo->data->t_die * 2);
+		pthread_mutex_unlock(first_fork);
 		return ;
 	}
-	pthread_mutex_lock(philo->right_fork);
+	pthread_mutex_lock(second_fork);
 	ft_print(philo, "has_taken_a_fork");
 }
-
 void	eat(t_philo *philo)
 {
 	t_data	*data;
+	pthread_mutex_t	*first_fork;
+	pthread_mutex_t	*second_fork;
 
 	data = philo->data;
+	if (philo->left_fork < philo->right_fork) {
+		first_fork = philo->left_fork;
+		second_fork = philo->right_fork;
+	} else {
+		first_fork = philo->right_fork;
+		second_fork = philo->left_fork;
+	}
 	ft_print(philo, "is_eating");
 	pthread_mutex_lock(&data->m_sim);
 	philo->last_meal = get_timestamp();
 	philo->n_meal++;
 	pthread_mutex_unlock(&data->m_sim);
 	ft_sleep(philo->data->t_eat);
-	pthread_mutex_unlock(philo->right_fork);
-	pthread_mutex_unlock(philo->left_fork);
+	pthread_mutex_unlock(second_fork);
+	pthread_mutex_unlock(first_fork);
 	ft_print(philo, "is_sleeping");
 	ft_sleep(philo->data->t_sleep);
 	ft_print(philo, "is_thinking");
